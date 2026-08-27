1c45dc0a8c3c39883c3249584b4e322c1c59ea20
Grojman <gomisromanjorge@gmail.com>
2026-08-27 14:01:54 +0200

    M

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { WebsocketService } from '../../core/services/websocket.service';
 import { Card } from '../../models/card.model';
 import { DeckInfo } from '../../models/deckinfo.model';
@@ -34,6 +34,7 @@ export class GalleryComponent implements OnInit {
       case "get_cards":
         this.cards = [...msg.Content];
         this.loading = false;
+        requestAnimationFrame(() => this.updateScrollFade());
         break;
       default:
         console.log("Unknown message!!");
@@ -44,5 +45,30 @@ export class GalleryComponent implements OnInit {
   }
 
   hoverCard: Card | null = null;
+
+  showScrollFade = false;
+
+  deckContent!: ElementRef<HTMLElement>;
+  @ViewChild('deckContent') set dc(dc: ElementRef)
+  {
+    if (dc)
+    {
+      this.deckContent = dc;
+      this.updateScrollFade();
+    }
+  }
+
+  async updateScrollFade() {
+    await requestAnimationFrame(() => {});
+    const el = this.deckContent?.nativeElement;
+    if (!el) return;
+
+    // Tolerance accounts for fractional-pixel rounding introduced by browser/OS zoom,
+    // which otherwise keeps scrollTop a hair short of the bottom forever.
+    const hasOverflow = el.scrollHeight - el.clientHeight > 1;
+    const distanceFromBottom = el.scrollHeight - el.scrollTop - el.clientHeight;
+
+    this.showScrollFade = hasOverflow && distanceFromBottom > 2;
+  }
 
 }
